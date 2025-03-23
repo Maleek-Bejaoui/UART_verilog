500bfb046167ab63f02eff15d2299260dda52f62
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-03-23 04:46:20 +0100

    Update top.v

--- src/top.v
+++ src/top.v
@@ -19,15 +19,15 @@ module tt_um_top (
 
   // All output pins must be assigned. If not used, assign to 0.
 //  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uio_out = 0;
+    assign uio_oe  = 0;
     assign uo_out[7:4]  = 4'b0000;
-  
+    assign uio_in  = 0;
+
     
 
   // List all unused inputs to prevent warnings
     wire _unused = &{ui_in[7:2], 6'b000000};
-   // wire _unused = &{uio_in,     8'b00000000};
 
     
    /* verilator lint_off PINCONNECTEMPTY */
