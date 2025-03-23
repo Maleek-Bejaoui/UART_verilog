bcd57bd0fc538138e83941f8167f33e337b1f63d
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-03-23 03:31:14 +0100

    Update top.v

--- src/top.v
+++ src/top.v
@@ -21,7 +21,7 @@ module tt_um_top (
 //  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-    assign uo_out[7:4]  = 5'b0000;
+    assign uo_out[7:4]  = 4'b0000;
   
     
 
